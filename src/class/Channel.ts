89cd4da8102a06ae9a24b71beaf0a0e452467db3
andjsrk <43586090+andjsrk@users.noreply.github.com>
2021-01-27 08:58:45 +0900

    violation of naming convention

--- src/class/Channel.ts
+++ src/class/Channel.ts
@@ -1,16 +1,16 @@
 import { GuildChannel } from 'discord.js'
 import BaseClass from './Base'
-import ChannelPermission from './ChannelPermission'
+import ChannelPermissionClass from './ChannelPermission'
 
 export default abstract class ChannelClass extends BaseClass {
 	readonly position: number
-	readonly permissions: ChannelPermission[]
+	readonly permissions: ChannelPermissionClass[]
 	readonly parent: GuildChannel | null
 	made: GuildChannel | null
 	constructor(channel: GuildChannel) {
 		super(channel)
 		this.position = channel.position
-		this.permissions = channel.permissionOverwrites.filter(permission => permission.type === 'role' && channel.guild.roles.cache.get(permission.id)!.editable).array().map(permission => new ChannelPermission(permission))
+		this.permissions = channel.permissionOverwrites.filter(permission => permission.type === 'role' && channel.guild.roles.cache.get(permission.id)!.editable).array().map(permission => new ChannelPermissionClass(permission))
 		if (channel.parent !== null) this.parent = channel.parent
 		else this.parent = null
 		this.made = null
