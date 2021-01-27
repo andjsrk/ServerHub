1cda30145034c7196e46773438474699a4b8ca03
andjsrk <43586090+andjsrk@users.noreply.github.com>
2021-01-27 23:11:10 +0900

    ?

--- src/util.ts
+++ src/util.ts
@@ -1,15 +1,6 @@
 import { ALPHABET } from './constants'
 
 export default class Util {
-	static checkNotVoid(value: unknown): value is notVoid {
-		return Util.checkNull(value) && Util.checkUndefined(value)
-	}
-	static checkUndefined(value: unknown): value is undefined {
-		return value !== undefined
-	}
-	static checkNull(value: unknown): value is null {
-		return value !== null
-	}
 	static makeId(length: number, idArray: string[]): string {
 		const JOINED_ALPHABET = `${ALPHABET.LOWER}${ALPHABET.UPPER}`
 		let id = ''
@@ -22,5 +13,3 @@ export default class Util {
 		return Math.floor(Math.random() * (max - min + 1)) + min
 	}
 }
-
-type notVoid = undefined!
